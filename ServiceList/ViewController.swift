0852182d7d9360504cd5205d376fccfb15cdae82
Muhamad Talebi <muhamad79t@gmail.com>
2023-05-22 13:44:00 +0330

    Initial Commit

---
+++ ServiceList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServiceList
+//
+//  Created by Muhamad Talebi on 5/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
